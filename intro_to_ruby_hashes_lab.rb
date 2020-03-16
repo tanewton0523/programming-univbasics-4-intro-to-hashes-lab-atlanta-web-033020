7b7cedbc625947bf773f050ca6530147afaaa133
TeMecha Newton <tanewton0523@gmail.com>
2020-03-16 21:19:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-   if hash[key] == nil
-     hash[key] = 1
- else hash[key] +=1
- end
+   if hash.key?(key)
+    hash[key] += 1 
+  else
+    hash[key] = 1
+  end
+  hash
 end
